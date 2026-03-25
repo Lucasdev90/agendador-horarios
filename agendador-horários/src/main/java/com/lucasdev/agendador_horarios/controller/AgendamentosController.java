baec5d36c0c29c9d2b2c6abe6acc1660900a4801
Lucas <slippain@gmail.com>
2026-03-24 23:40:02 -0300

    adicionado projeto agendador horarios

---
+++ agendador-horários/src/main/java/com/lucasdev/agendador_horarios/controller/AgendamentosController.java
@@ -0,0 +1,51 @@
+package com.lucasdev.agendador_horarios.controller;
+
+import com.lucasdev.agendador_horarios.infrastructure.entity.Agendamento;
+import com.lucasdev.agendador_horarios.services.AgendamentoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("/agendamentos")
+@RequiredArgsConstructor
+
+public class AgendamentosController {
+
+    private final AgendamentoService agendamentoService;
+
+    @PostMapping
+
+    public ResponseEntity<Agendamento> salvarAgendamento(@RequestBody Agendamento agendamento){
+        return ResponseEntity.accepted().body(agendamentoService.salvarAgendamento(agendamento));
+    }
+
+    @DeleteMapping
+
+    public ResponseEntity<Void>deletarAgendamento(@RequestParam String cliente,
+                                                   @RequestParam LocalDateTime dataHoraAgendamento){
+
+        agendamentoService.deletarAgendamento(dataHoraAgendamento, cliente);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+
+    public ResponseEntity<Agendamento> buscarAgendamentosDia(@RequestParam LocalDate data){
+        return ResponseEntity.ok().body(agendamentoService.buscarAgendamentosDia(data));
+    }
+
+    @PutMapping
+
+    public ResponseEntity<Agendamento> alterarAgendamentos(@RequestBody Agendamento agendamento,
+                                                           @RequestParam String cliente,
+                                                           @RequestParam LocalDateTime dataHoraAgendamento){
+        return ResponseEntity.accepted().body(agendamentoService.alterarAgendamento
+                                                            (agendamento, cliente, dataHoraAgendamento));
+    }
+
+}
---
+++ agendador-horários/src/main/java/com/lucasdev/agendador_horarios/infrastructure/repository/AgendamentoRepository.java
@@ -0,0 +1,28 @@
+package com.lucasdev.agendador_horarios.infrastructure.repository;
+
+import com.lucasdev.agendador_horarios.infrastructure.entity.Agendamento;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+
+public interface AgendamentoRepository extends JpaRepository<Agendamento, Long > {
+
+
+    Agendamento findByDataHoraAgendamentoAndCliente(LocalDateTime dataHoraAgendamento, String cliente);
+
+    Agendamento findByServicoAndDataHoraAgendamentoBetween(String servico, LocalDateTime dataHoraInicio, LocalDateTime dataHoraFinal);
+
+
+    @Transactional
+    void deleteByDataHoraAgendamentoAndCliente
+            (LocalDateTime dataHoraAgendamento,String cliente );
+
+
+    Agendamento findByDataHoraAgendamentoBetween(LocalDateTime dataHoraInicial, LocalDateTime dataHoraFinal);
+
+
+}
+
+
+
